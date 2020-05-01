be1ca246276ee56ed839bfced436f14af941ebb3
AudioHouse <austinmiller360@gmail.com>
2020-05-01 15:04:04 -0700

    Initial commit for backend

---
+++ backend/src/main/java/com/audiohouse/notarytracker/NotaryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.audiohouse.notarytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaryTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/audiohouse/notarytracker/NotaryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audiohouse.notarytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
