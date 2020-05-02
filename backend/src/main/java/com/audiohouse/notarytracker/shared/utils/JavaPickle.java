2468faef723b925d648974be04e6ae2e9545d6f0
AudioHouse <austinmiller360@gmail.com>
2020-05-01 20:50:31 -0700

    Refactor writeFile in JavaPickle

--- backend/src/main/java/com/audiohouse/notarytracker/shared/utils/JavaPickle.java
+++ backend/src/main/java/com/audiohouse/notarytracker/shared/utils/JavaPickle.java
@@ -19,18 +19,31 @@ public class JavaPickle {
     private static final Object lock = new Object();
 
     public <T> T save(T objectToSave, String objectId, String fileName) {
-        // first try to retrieve the current set of objects
+        HashMap<String, T> currentSet;
+        try {
+            // first try to retrieve the current set of objects
+            currentSet = this.getHashMap(fileName);
+        } catch (FileNotFoundException e) {
+            // If file does not exist, we will create a new one
+            currentSet = new HashMap<>();
+            currentSet.put(objectId, objectToSave);
+        }
+        return this.writeFile(objectToSave, objectId, fileName, currentSet);
+    }
+
+    public <T> List<T> getAll(String fileName) {
+        List<T> listToReturn = new ArrayList<>();
         HashMap<String, T> currentSet;
         try {
             currentSet = this.getHashMap(fileName);
-            return this.writeToExistingFile(objectToSave, objectId, fileName, currentSet);
         } catch (FileNotFoundException e) {
-            // If file does not exist, we will create a new one
-            return this.writeToNewFile(objectToSave, objectId, fileName);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There are no entries in database");
         }
+        currentSet.forEach((key, value) -> listToReturn.add(value));
+        return listToReturn;
     }
 
-    private <T> T writeToExistingFile(T objectToSave, String objectId, String fileName, HashMap<String, T> currentSet) {
+    private <T> T writeFile(T objectToSave, String objectId, String fileName, HashMap<String, T> currentSet) {
         synchronized (lock) {
             FileOutputStream fos;
             ObjectOutputStream oos;
@@ -46,38 +59,6 @@ public class JavaPickle {
             }
             return objectToSave;
         }
-    }
-
-    private <T> T writeToNewFile(T objectToSave, String objectId, String fileName) {
-        synchronized (lock) {
-            FileOutputStream fos;
-            ObjectOutputStream oos;
-            HashMap<String, T> newSet = new HashMap<>();
-            newSet.put(objectId, objectToSave);
-            try {
-                fos = new FileOutputStream(fileName);
-                oos = new ObjectOutputStream(fos);
-                newSet.put(objectId, objectToSave);
-                oos.writeObject(newSet);
-                oos.close();
-                oos.close();
-            } catch (IOException e) {
-                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-            }
-            return objectToSave;
-        }
-    }
-
-    public <T> List<T> getAll(String fileName) {
-        List<T> listToReturn = new ArrayList<>();
-        HashMap<String, T> currentSet;
-        try {
-            currentSet = this.getHashMap(fileName);
-        } catch (FileNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There are no entries in database");
-        }
-        currentSet.forEach((key, value) -> listToReturn.add(value));
-        return listToReturn;
     }
 
     private <T> HashMap<String, T> getHashMap(String fileName) throws FileNotFoundException {
