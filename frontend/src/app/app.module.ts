03e4cdea6af8bdf05103b4e34f1dffe1f2568fe2
AudioHouse <austinmiller360@gmail.com>
2020-05-03 16:40:35 -0700

    Generate components for login-page and main-navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './main/homepage/homepage.component';
+import { LoginPageComponent } from './main/login-page/login-page.component';
+import { MainNavbarComponent } from './main/components/main-navbar/main-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginPageComponent,
+    MainNavbarComponent
   ],
   imports: [
     BrowserModule,
