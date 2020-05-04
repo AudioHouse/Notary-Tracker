4dc197ad0c787efb05243f4c581c6b2f4643dc15
AudioHouse <austinmiller360@gmail.com>
2020-05-03 17:59:52 -0700

    Begin structuring front-end project

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './main/homepage/homepage.component';
+import { LoginPageComponent } from './main/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent},
+  { path: 'login', component: LoginPageComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
