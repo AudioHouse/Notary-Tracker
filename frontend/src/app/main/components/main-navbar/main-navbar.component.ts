6286c1f6781fc5cf492d4eb8949d6841a0edadd2
AudioHouse <austinmiller360@gmail.com>
2020-05-08 23:14:21 -0700

    Swap anchors for ngb buttons

--- frontend/src/app/main/components/main-navbar/main-navbar.component.ts
+++ frontend/src/app/main/components/main-navbar/main-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-navbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goHome(): void {
+    this.router.navigate(["/home"]);
+  }
+
+  goSchedule(): void {
+    this.router.navigate(["/login"]);
   }
 
 }
