d32f82f9b50db5edab97c5f0a1b9c6d48896db34
AudioHouse <austinmiller360@gmail.com>
2020-05-21 14:48:55 -0700

    Add main guard

---
+++ frontend/src/app/guards/main.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private jwtHelper: JwtHelperService) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+
+    const authToken: string = localStorage.getItem('access_token');
+
+    if (authToken == null) {
+      this.router.navigate(['/login']);
+      return false;
+    } else if (this.jwtHelper.isTokenExpired(authToken)) {
+      this.router.navigate(['/login']);
+      this.toastr.warning('Your user session has expired', 'Session Expired');
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
