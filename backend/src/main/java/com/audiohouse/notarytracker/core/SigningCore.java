92d1cbf15409f2098777982a8252fcd8ac1d503c
AudioHouse <austinmiller360@gmail.com>
2020-05-30 17:04:09 -0700

    Implement get signing by norary id

--- backend/src/main/java/com/audiohouse/notarytracker/core/SigningCore.java
+++ backend/src/main/java/com/audiohouse/notarytracker/core/SigningCore.java
@@ -7,6 +7,9 @@ import com.audiohouse.notarytracker.shared.utils.JavaPickle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SigningCore {
 
     private static final Logger logger = LoggerFactory.getLogger(SigningCore.class);
@@ -23,5 +26,25 @@ public class SigningCore {
         // create signing
         SigningEntity entityToCreate = EntityTransformer.newSigningEntityFromPostSigning(postSigning);
         return jPickle.save(entityToCreate, entityToCreate.getSigningId(), signingFileLocation);
+    }
+
+    public SigningEntity getSigningById(String signingId) {
+        logger.info("Getting signing by id: {}", signingId);
+        // get signing by id
+        return jPickle.getById(signingId, signingFileLocation);
+    }
+
+    public List<SigningEntity> getSigningsListByNotaryId(String notaryId) {
+        logger.info("Getting list of signings with notary id: {}", notaryId);
+        List<SigningEntity> listToReturn = new ArrayList<>();
+        // get all signings
+        List<SigningEntity> listOfAllSignings = jPickle.getAll(signingFileLocation);
+        // iterate through and grab matching ones
+        for (SigningEntity signing : listOfAllSignings) {
+            if (signing.getAssignedNotaryId().equals(notaryId)) {
+                listToReturn.add(signing);
+            }
+        }
+        return listToReturn;
     }
 }
